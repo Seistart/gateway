e2a148bc7936fff41177927b62a6afdbbf582328
Haxfx <sebastiaan@hotflux.nl>
2024-02-27 22:54:36 +0100

    fix(projects): adjusted msw mocked data (#4)

--- src/app/(app)/projects/page.tsx
+++ src/app/(app)/projects/page.tsx
@@ -1,7 +1,7 @@
 import { appMetadata } from '@/config';
 import { Projects } from '@/components/pages'
 import { getProjects } from '@/lib/api/projects/queries';
-import { getMockProjects } from '@/lib/mock-data';
+import { mockProjects } from '@/lib/mock-data';
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -10,10 +10,6 @@ export const metadata: Metadata = {
 
 export default async function ProjectsPage() {
   const { projects } = await getProjects();
-
-  const mockProjects = getMockProjects();
-
-  console.log(mockProjects)
   
   return <Projects projects={mockProjects} />
 }
