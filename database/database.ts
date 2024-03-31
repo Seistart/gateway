446658305820aca34d29629ad3ac524aa2a3c9d6
Daniel Berger <daniel.berger95@gmail.com>
2024-03-30 21:11:55 -0400

    add back local database

--- database/database.ts
+++ database/database.ts
@@ -7,3 +7,10 @@ export const db = drizzle(new RDSDataClient({}), {
   secretArn: Resource.SeistartDatabase.secretArn,
   resourceArn: Resource.SeistartDatabase.clusterArn,
 })
+
+// For Local
+// import { env } from "@/env.mjs"
+// import { drizzle } from "drizzle-orm/postgres-js"
+// import postgres from "postgres"
+// export const client = postgres(env.DATABASE_URL)
+// export const db = drizzle(client)
